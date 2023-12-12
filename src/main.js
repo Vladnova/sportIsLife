b49c74c620996b2c221f167e05ddbd65fa2374bf
Nadiia Honcharova <86801593+NGanch@users.noreply.github.com>
2023-12-12 23:04:34 +0000

    comit (#11)
    * Update exercises.js
    
    ---------
    
    Co-authored-by: Vladislav Novalenko <61030713+Vladnova@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { googleSignin, logOut } from './js/authentication.js';
 import { openMenu, closeMenu } from './js/burger';
 import { chengeOnActive } from './js/header.js';
+import './js/muscles.js';
+import './js/exercises';
 
 import fetchSportEnergy from './shared/api/apiSport';
+
 
 export { googleSignin, logOut, openMenu, closeMenu, chengeOnActive };
 
