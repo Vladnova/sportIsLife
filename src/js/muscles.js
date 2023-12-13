9b0c60d235580d0b27c6be53b1f5b8642de74e7e
NGanch <86801593+NGanch@users.noreply.github.com>
2023-12-13 22:01:49 +0000

    comit

--- src/js/muscles.js
+++ src/js/muscles.js
@@ -1,56 +1,75 @@
 import fetchSportEnergy from './api/apiSport';
 
-import { getDataExercises } from './exercises';
+import { handleCategoryClick } from './search';
 
-const categoryList = document.querySelector('.wrap-button');
-
-
-categoryList.addEventListener('click', handleCategoryClick);
-
-export async function handleCategoryClick(event) {
-  if (event.target.nodeName !== 'BUTTON') {
-    return;
-  }
-  event.preventDefault();
-  const target = event.target;
-  const categoryName = target.textContent;
-
-    // if(target.classList.contains('btn-filter')){
-    //     const categoryName = target.textContent;
-    //     console.log(categoryName);
-    //     const categoryItem = document.querySelectorAll('wrap-list-btn');
-    //     categoryItem.forEach(item => {
-    //         item.classList.remove('btn-filter-active')
-    //     })
-    // }
-    // // Добавление класса 'active' к текущему пункту меню
-    // target.parentNode.classList.add('btn-filter-active');
-
-  if (categoryName === 'Body parts') {
-    musclesList.innerHTML = '';
-    getDataExercises();
-  } else {
-    musclesList.innerHTML = '';
-    loadSectionOnClick(categoryName);
-  }
-}
-//======================================================
 const musclesList = document.querySelector('.muscles-list');
 
-window.addEventListener('DOMContentLoaded', loadSectionOnClick);
+const musclesBtn = document.querySelector('#Muscles');
 
-async function loadSectionOnClick(categoryName) {
+const bodyParts = document.querySelector('#Body parts');
+
+const Equipment = document.querySelector('#Equipment');
+
+// categoryList.addEventListener('click', handleCategoryClick);
+
+handleCategoryClick();
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadSectionOnClick({ filter: 'Muscles', page: 1, limit: 12 }, 'Muscles');
+});
+
+// export async function handleCategoryClick(event) {
+//   event.preventDefault();
+//   const target = event.target;
+//   const categoryName = target.textContent;
+
+//   // Видалення класу btn-filter-active з musclesBtn
+//   musclesBtn.classList.remove('btn-filter-active');
+
+//   if (target.classList.contains('btn-filter')) {
+//     const categoryItem = document.querySelectorAll('.wrap-list-btn');
+//     categoryItem.forEach(item => {
+//       item.classList.remove('btn-filter-active');
+//     });
+//   }
+
+//   // Добавление класса 'active' к текущему пункту меню
+//   target.parentNode.classList.add('btn-filter-active');
+
+//   const dataFilter = {
+//     filter: categoryName,
+//     page: 1,
+//     limit: 12,
+//   };
+//   musclesList.innerHTML = '';
+//   loadSectionOnClick(dataFilter, categoryName);
+// }
+
+//======================================================
+
+export async function loadSectionOnClick(dataFilter) {
   try {
-    const filter = await fetchSportEnergy.getByFilterName(categoryName);
+    const filter = await fetchSportEnergy.getByFilterName(dataFilter);
     console.log('filter-->', filter);
 
     const filteredResult = filter.results;
 
-    if (filter.length === 0 || filter === undefined) {
+    if (!filter || filteredResult.length === 0) {
       console.log("Sorry, we didn't find anything according to your request.");
       return;
     }
     musclesList.insertAdjacentHTML('beforeend', makeMarkupMuscles(filteredResult));
+
+    // Перевірка категорії та додавання класу btn-filter-active
+    if (dataFilter.filter === 'Muscles') {
+      musclesBtn.classList.add('btn-filter-active');
+    } else if (dataFilter.filter === 'Body parts') {
+      musclesBtn.classList.remove('btn-filter-active');
+      bodyParts.classList.add('btn-filter-active');
+    } else if (dataFilter.filter === 'Equipment') {
+      bodyParts.classList.remove('btn-filter-active');
+      Equipment.classList.add('btn-filter-active');
+    }
   } catch (error) {
     console.log(error.message);
   }
