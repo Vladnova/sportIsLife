9915fb3921552ca909504522e3de562faef92926
Mamrenko-Alex <Mamrenkodev@yandex.com>
2023-12-13 23:37:40 +0100

    adaptation modal window is done

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { googleSignin, logOut } from './js/authentication.js';
 import { openMenu, closeMenu } from './js/burger';
 import { chengeOnActive } from './js/header.js';
+import './js/modal-window/modal.js'
 import './js/muscles.js';
 import './js/exercises';
 import './js/pagination/pagination.js'
