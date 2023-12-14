2402f137f451cd51b3aa37f8c9186ed4e19bbeea
Nadiia Honcharova <86801593+NGanch@users.noreply.github.com>
2023-12-14 15:34:42 +0000

    comit (#29)

--- src/js/muscles.js
+++ src/js/muscles.js
@@ -1,8 +1,8 @@
 import fetchSportEnergy from './api/apiSport';
 
-import { handleCategoryClick } from './search';
-
 import * as pagination from './pagination/pagination';
+
+const categoryList = document.querySelector('.wrap-button');
 
 const musclesList = document.querySelector('.muscles-list');
 
@@ -12,41 +12,40 @@ const bodyParts = document.querySelector('#Body parts');
 
 const Equipment = document.querySelector('#Equipment');
 
-// categoryList.addEventListener('click', handleCategoryClick);
+categoryList.addEventListener('click', handleCategoryClick);
 
-handleCategoryClick();
 
 document.addEventListener('DOMContentLoaded', () => {
   loadSectionOnClick({ filter: 'Muscles', page: 1, limit: 12 }, 'Muscles');
 });
 const totalPages = 5; // потрібно видалити після прокиндання з ф-ї loadSectionOnClick filter.totalPages
 
-// export async function handleCategoryClick(event) {
-//   event.preventDefault();
-//   const target = event.target;
-//   const categoryName = target.textContent;
+export async function handleCategoryClick(event) {
+  event.preventDefault();
+  const target = event.target;
+  const categoryName = target.dataset.name;
+console.log(categoryName)
+  // Видалення класу btn-filter-active з musclesBtn
+  musclesBtn.classList.remove('btn-filter-active');
 
-//   // Видалення класу btn-filter-active з musclesBtn
-//   musclesBtn.classList.remove('btn-filter-active');
+  if (target.classList.contains('btn-filter')) {
+    const categoryItem = document.querySelectorAll('.wrap-list-btn');
+    categoryItem.forEach(item => {
+      item.classList.remove('btn-filter-active');
+    });
+  }
 
-//   if (target.classList.contains('btn-filter')) {
-//     const categoryItem = document.querySelectorAll('.wrap-list-btn');
-//     categoryItem.forEach(item => {
-//       item.classList.remove('btn-filter-active');
-//     });
-//   }
+  // Добавление класса 'active' к текущему пункту меню
+  target.parentNode.classList.add('btn-filter-active');
 
-//   // Добавление класса 'active' к текущему пункту меню
-//   target.parentNode.classList.add('btn-filter-active');
-
-//   const dataFilter = {
-//     filter: categoryName,
-//     page: 1,
-//     limit: 12,
-//   };
-//   musclesList.innerHTML = '';
-//   loadSectionOnClick(dataFilter, categoryName);
-// }
+  const dataFilter = {
+    filter: categoryName,
+    page: 1,
+    limit: 12,
+  };
+  musclesList.innerHTML = '';
+  loadSectionOnClick(dataFilter, categoryName);
+}
 
 //======================================================
 
