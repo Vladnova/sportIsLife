29421637aa1ae903bacd5bb2d28d0f27c0d6b8b6
Vladislav Novalenko <61030713+Vladnova@users.noreply.github.com>
2023-12-15 01:31:45 +0200

    fix-pagination (#34)
    Co-authored-by: Vladyslav Novalenko <vladyslav.novalenko@infotech.gov.ua>

--- src/js/muscles.js
+++ src/js/muscles.js
@@ -1,42 +1,49 @@
 import fetchSportEnergy from './api/apiSport';
-
 import * as pagination from './pagination/pagination';
-
 const categoryList = document.querySelector('.wrap-button');
 
 const musclesList = document.querySelector('.muscles-list');
+const paginationNumbers = document.querySelector('.pagination-numbers');
 
-const musclesBtn = document.querySelector('#Muscles');
+// const musclesBtn = document.querySelector('#Muscles');
 
-const bodyParts = document.querySelector('#Body parts');
+// const bodyParts = document.querySelector('#Body parts');
 
-const Equipment = document.querySelector('#Equipment');
+// const Equipment = document.querySelector('#Equipment');
 
 categoryList.addEventListener('click', handleCategoryClick);
 
 
 document.addEventListener('DOMContentLoaded', () => {
   loadSectionOnClick({ filter: 'Muscles', page: 1, limit: 12 }, 'Muscles');
+
+
 });
-const totalPages = 5; // потрібно видалити після прокиндання з ф-ї loadSectionOnClick filter.totalPages
+
 
 export async function handleCategoryClick(event) {
   event.preventDefault();
-  const target = event.target;
+  const {target, currentTarget} = event;
+
+  if(target.nodeName!=='BUTTON') return;
   const categoryName = target.dataset.name;
-console.log(categoryName)
-  // Видалення класу btn-filter-active з musclesBtn
-  musclesBtn.classList.remove('btn-filter-active');
 
   if (target.classList.contains('btn-filter')) {
-    const categoryItem = document.querySelectorAll('.wrap-list-btn');
+    const categoryItem =[...currentTarget.children];
     categoryItem.forEach(item => {
-      item.classList.remove('btn-filter-active');
+      item.firstElementChild.classList.remove('btn-filter-active');
     });
   }
 
+  // if (target.classList.contains('btn-filter')) {
+  //   const categoryItem = document.querySelectorAll('.wrap-list-btn');
+  //   categoryItem.forEach(item => {
+  //     item.classList.remove('btn-filter-active');
+  //   });
+  // }
+
   // Добавление класса 'active' к текущему пункту меню
-  target.parentNode.classList.add('btn-filter-active');
+  target.classList.add('btn-filter-active');
 
   const dataFilter = {
     filter: categoryName,
@@ -44,42 +51,53 @@ console.log(categoryName)
     limit: 12,
   };
   musclesList.innerHTML = '';
-  loadSectionOnClick(dataFilter, categoryName);
+  loadSectionOnClick(dataFilter);
 }
 
 //======================================================
 
 export async function loadSectionOnClick(dataFilter) {
+  console.log(dataFilter)
   try {
     const filter = await fetchSportEnergy.getByFilterName(dataFilter);
-    console.log('filter-->', filter);
-
     const filteredResult = filter.results;
 
     if (!filter || filteredResult.length === 0) {
       console.log("Sorry, we didn't find anything according to your request.");
       return;
     }
-    
-    // Перевірка категорії та додавання класу btn-filter-active
-    if (dataFilter.filter === 'Muscles') {
-      musclesBtn.classList.add('btn-filter-active');
-    } else if (dataFilter.filter === 'Body parts') {
-      musclesBtn.classList.remove('btn-filter-active');
-      bodyParts.classList.add('btn-filter-active');
-    } else if (dataFilter.filter === 'Equipment') {
-      bodyParts.classList.remove('btn-filter-active');
-      Equipment.classList.add('btn-filter-active');
-    }
-    
+
     musclesList.insertAdjacentHTML('beforeend', makeMarkupMuscles(filteredResult));
+
+
+
+    // Збереження в LocalStorage інформації для пагінації сторінки
+    const {totalPages} = filter;
+    const data = JSON.stringify({totalPages, categoryName:dataFilter.filter});
+    localStorage.setItem('infoRequest', data);
+    paginationNumbers.innerHTML = '';
+    pagination.getPaginationNumbers();
+
     pagination.setCurrentPage(1);
+
+
+    // Перевірка категорії та додавання класу btn-filter-active
+    // if (dataFilter.filter === 'Muscles') {
+    //   musclesBtn.classList.add('btn-filter-active');
+    // } else if (dataFilter.filter === 'Body parts') {
+    //   musclesBtn.classList.remove('btn-filter-active');
+    //   bodyParts.classList.add('btn-filter-active');
+    // } else if (dataFilter.filter === 'Equipment') {
+    //   bodyParts.classList.remove('btn-filter-active');
+    //   Equipment.classList.add('btn-filter-active');
+    // }
   } catch (error) {
     console.log(error.message);
   }
 }
 
-pagination.getPaginationNumbers(totalPages); // потрібно прокинути з ф-ї loadSectionOnClick filter.totalPages
+
+
 
 export function makeMarkupMuscles(filteredResult) {
   const markup = filteredResult
