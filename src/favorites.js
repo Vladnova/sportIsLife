c4e0fe9237f5abe32b639c544c6c3a81465c8468
Vladislav Novalenko <61030713+Vladnova@users.noreply.github.com>
2023-12-17 01:13:16 +0200

    add markup (#73)
    Co-authored-by: Vladyslav Novalenko <vladyslav.novalenko@infotech.gov.ua>

---
+++ src/favorites.js
@@ -0,0 +1,11 @@
+import './js/header.js';
+import './js/burger.js';
+import './js/authentication.js';
+import './js/favorite/favorite.js'
+import './js/modal-window/modal.js';
+// import './js/quote.js';
+
+// import './js/pagination/pagination.js';
+import './js/scrollToTopButton.js';
+import './js/exercises-right-part-filter.js';
+import './js/raiting.js';
