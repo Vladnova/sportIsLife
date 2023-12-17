18473c9f6db9595426f5b5cea5e773deb8400a46
Iryna <100072177+IrynaSabinina@users.noreply.github.com>
2023-12-17 17:39:33 +0100

    update btn

--- src/js/header.js
+++ src/js/header.js
@@ -5,6 +5,8 @@ const Favorites = document.getElementById("favorites")
 const TextHome = document.getElementById("home-text")
 const TextFavorite = document.getElementById("favorites-text")
 const Nav = document.getElementById("navigation")
+const inBtn = document.getElementById('sign-in-js')
+const outBtn = document.getElementById('sign-out')
 const currentUrl = window.location.href.toString();
 
 let part = currentUrl.slice(-14)
@@ -16,6 +18,8 @@ export const onClickPage = () => {
       TextHome.classList.remove("black");
       Favorites.classList.add("active");
       TextFavorite.classList.add("black");
+      inBtn.classList.add("visually-hidden")
+      outBtn.classList.add("visually-hidden")
       addContent()
    } else {
       Home.classList.add("active");
