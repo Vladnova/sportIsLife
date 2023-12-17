ecc9f379858f09287b127dd9fb2ef1977e1d894e
Vladyslav Novalenko <vladyslav.novalenko@infotech.gov.ua>
2023-12-17 04:02:00 +0200

    fix-favorite

--- src/js/favorite/markup-text.js
+++ src/js/favorite/markup-text.js
@@ -1,6 +1,9 @@
 export function markupText() {
+  const list = document.querySelector('.filter-list-js');
+  list.classList.add('hidden-form');
   const parentEl = document.querySelector('.wrap-js')
   const subtitle = document.createElement('p');
+  subtitle.className = 'favorite-text';
   parentEl.appendChild(subtitle);
   subtitle.textContent ="It appears that you haven`t added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future."
 }
