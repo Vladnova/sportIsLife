1cbd5dfc8ef910da21e2a82f577c11a4e9cfa605
Iryna <100072177+IrynaSabinina@users.noreply.github.com>
2023-12-17 16:26:53 +0100

    Sabinina2 (#81)
    * update firebase
    
    * some updates exercises
    
    * update
    
    * update
    
    * update
    
    ---------
    
    Co-authored-by: Vladislav Novalenko <61030713+Vladnova@users.noreply.github.com>

--- src/js/utils/localStor.js
+++ src/js/utils/localStor.js
@@ -1,13 +1,26 @@
+import { deleteFavorites, updateFirestore } from "./firebase";
+
 let favoriteCards = [];
 const localStorFavorites = JSON.parse(localStorage.getItem('favorites'));
 localStorFavorites?.length && favoriteCards.push(...localStorFavorites);
 
 export function addLocalFavorites(card) {
-  favoriteCards.push(card);
-  localStorage.setItem('favorites', JSON.stringify(favoriteCards));
+  const user=localStorage.getItem("user");
+  
+  if(user){
+    updateFirestore(card)
+  } if (!user){
+
+    favoriteCards.push(card);
+    localStorage.setItem('favorites', JSON.stringify(favoriteCards));
+  }
 }
 
 export const deleteLocalFavorites = id => {
+  const user=localStorage.getItem("user");
+  if(user){
+    deleteFavorites(id)
+  }
   favoriteCards = favoriteCards.filter(item => item._id !== id);
 
   localStorage.setItem(
