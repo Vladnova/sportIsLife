5402c02e9f90ba30445eca1a8b3daf130dfec855
Mamrenko-Alex <Mamrenkodev@yandex.com>
2023-12-18 17:22:24 +0100

    Update filter.js

--- src/js/filter.js
+++ src/js/filter.js
@@ -15,6 +15,25 @@ document.addEventListener('DOMContentLoaded', () => {
   loadSectionOnClick({ filter: 'Muscles', page: 1, limit: 12 }, 'Muscles');
 });
 
+// Функція, яка перевіряє, чи прокручено сторінку до кінця
+function isPageScrolledToBottom() {
+  // Поточна позиція прокрутки
+  var scrollTop = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+
+  // Висота всього документа
+  var documentHeight = Math.max(
+    document.body.scrollHeight, document.documentElement.scrollHeight,
+    document.body.offsetHeight, document.documentElement.offsetHeight,
+    document.body.clientHeight, document.documentElement.clientHeight
+  );
+
+  // Висота вікна браузера
+  var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+  // Перевірка, чи прокручено до кінця сторінки (з іншими умовами, які вам можуть знадобитися)
+  return scrollTop + windowHeight >= documentHeight;
+}
+
 export async function handleCategoryClick(event) {
   event.preventDefault();
   exercisesTag.innerHTML = 'Exercises';
@@ -55,7 +74,16 @@ export async function loadSectionOnClick(dataFilter) {
       return;
     }
     musclesList.insertAdjacentHTML('beforeend', makeMarkupMuscles(filteredResult))
-    makeMarkupMuscles(filteredResult);
+
+    // Прослуховування подій прокрутки
+    window.addEventListener('scroll', function () {
+      // Якщо сторінка прокручена до кінця, викликаємо певні дії
+      if (isPageScrolledToBottom()) {
+        console.log('Сторінка завантажилась до кінця!');
+        makeMarkupMuscles(filteredResult);
+        // Тут ви можете викликати функції або робити інші дії
+      }
+    });
 
     // Збереження в LocalStorage інформації для пагінації сторінки
     const { totalPages } = filter;
